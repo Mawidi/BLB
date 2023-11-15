887c1d7742f333d4c5aa0003fd2d4c5cbdbaefc3
Mawidi <91553452+Mawidi@users.noreply.github.com>
2023-11-15 19:55:19 +0000

    BLB

--- server.js
+++ server.js
@@ -1,93 +1,105 @@
-//this is where your express sever is created and managed
-
-//do not forget to import the necessary modules,
-
-
-//do not forget to connect to your mongoDb
-
-
-
-//do not forget to set up routes to serve out the html file
-
-// Do not forget to handle your form posts for the relevant forms
-
-// Do not forget to handle form gets for the necessary data to display on the user interface.
-
-
-//Do not forget to catch all the erros that may arise during the process of parsing data from one form to another.
-
-// do not forget to create a port for your application and serving out the appliacation via the port created 
 const express = require('express');
 const mongoose = require('mongoose');
-const BLB = require('./BLB-MAIN');
-
+const bodyParser = require('body-parser');
 const app = express();
-const port = process.env.PORT || 3000;
 
-// MongoDB connection
-const username = 'nathan';
-const password = 'NA3108tha..';
-const databaseName = 'BLB';
-
-mongoose.connect('mongodb+srv://nathan:NA3108tha..@cluster0.21pluz0.mongodb.net/BLB?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+// to the MongoDB
+mongoose.connect('mongodb+srv://nathan:NA3108tha..@cluster0.hqy9pky.mongodb.net/BLB?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
+// Models
+// Models
+const { Citizen, Title } = require('./models'); // Update the require statement
 
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// Define MongoDB schema
-const personSchema = new mongoose.Schema({
-  name: String,
-  dob: Date,
-  fatherName: String,
-  motherName: String,
-  gender: String,
-  bloodGroup: String,
-});
+// Serve static files
+app.use(express.static('public'));
 
-const Person = mongoose.model('Person', personSchema);
-
-// Middleware to parse request bodies
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Serve the HTML file
+// Index route
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/your-html-file.html');
+  // Render your HTML page here
+  res.sendFile(__dirname + '/index.html');
 });
 
-// Handle form post for the personal information form
-app.post('/personal-info', (req, res) => {
-  const personData = req.body;
-
-  // Create a new Person document and save it to the database
-  const newPerson = new Person(personData);
-  newPerson.save((err) => {
-    if (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-    } else {
-      res.send('Personal information saved successfully!');
-    }
+// Citizen registration route
+app.post('/registerCitizen', (req, res) => {
+  const { name, dob, fatherName, motherName, gender, bloodGroup } = req.body;
+  
+  // Create a new Citizen instance
+  const newCitizen = new Citizen({
+    name,
+    dob,
+    fatherName,
+    motherName,
+    gender,
+    bloodGroup
   });
+
+  // Save the new citizen to the database
+  newCitizen.save()
+    .then(citizen => res.send(citizen))
+    .catch(err => res.status(500).send(err));
 });
 
-// Handle form get for the title search
-app.get('/title-search', (req, res) => {
-  const titleNumber = req.query.titleNumber;
+// Title registration route
+app.post('/registerTitle', (req, res) => {
+  const { ownerName, location, size, coordinates, titleNumber, photo } = req.body;
+  
+  // Create a new Title instance
+  const newTitle = new Title({
+    ownerName,
+    location,
+    size,
+    coordinates,
+    titleNumber,
+    photo
+  });
 
-  // Perform the title search logic here
-  // ...
-
-  res.send(`Title search for ${titleNumber}`);
+  // Save the new title to the database
+  newTitle.save()
+    .then(title => res.send(title))
+    .catch(err => res.status(500).send(err));
 });
 
-// Handle errors
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something went wrong!');
+// Fetch all registered citizens
+app.get('/registeredCitizens', (req, res) => {
+  Citizen.find()
+    .then(citizens => res.send(citizens))
+    .catch(err => res.status(500).send(err));
 });
 
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+// Fetch all registered titles
+app.get('/registeredTitles', (req, res) => {
+  Title.find()
+    .then(titles => res.send(titles))
+    .catch(err => res.status(500).send(err));
 });
+
+// Title transfer route
+app.post('/titleTransfer', (req, res) => {
+  const { titleNumber, newOwnerName } = req.body;
+
+  // Find the title by title number
+  Title.findOne({ titleNumber })
+    .then(title => {
+      if (!title) {
+        return res.status(404).send('Title not found');
+      }
+
+      // Update the owner name
+      title.ownerName = newOwnerName;
+
+      // Save the updated title
+      title.save()
+        .then(updatedTitle => res.send(updatedTitle))
+        .catch(err => res.status(500).send(err));
+    })
+    .catch(err => res.status(500).send(err));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
